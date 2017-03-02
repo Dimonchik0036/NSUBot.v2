e4d3c1dae9b38955c12878d9c6813492506b2a76
Dimonchik0036 <dimonchik0036@gmail.com>
2017-03-03 02:19:59 +0700

    Рефакторинг

--- bot/bot.go
+++ bot/bot.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"TelegramBot/loader"
+	"TelegramBot/schedule"
 	"TelegramBot/weather"
-	"errors"
 	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"golang.org/x/net/html"
-	"golang.org/x/net/html/charset"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"regexp"
 	"strconv"
 	"time"
 )
@@ -19,7 +14,7 @@ import (
 var chats = make(map[int64]string)
 var users = make(map[int]string)
 var userGroup = make(map[int]string)
-var schedule = make(map[string][7]string)
+var scheduleMap = make(map[string][7]string)
 
 var chatsCount int
 var usersCount int
@@ -45,10 +40,10 @@ func defaultUserSchedule(id int, group string) string {
 		return "Слишком много символов."
 	}
 
-	_, ok := schedule[group]
+	_, ok := scheduleMap[group]
 	if !ok {
 		group += ".1"
-		_, ok = schedule[group]
+		_, ok = scheduleMap[group]
 		if !ok {
 			return "Введён некорректный номер группы, попробуйте повторить попытку или воспользоваться /help и /faq для помощи."
 		}
@@ -57,106 +52,6 @@ func defaultUserSchedule(id int, group string) string {
 	userGroup[id] = group
 
 	return "Группа '" + group + "' успешно назначена, нажмите на /today или /tomorrow для проверки правильности выбора."
-}
-
-func parseSchedule() {
-	for {
-		res, err := http.Get("http://www.nsu.ru/education/schedule/Html_GK/Groups/")
-		if err != nil {
-			logAll.Print("Не удалось загрузить страницу:", err)
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		if res.Status != "200 OK" {
-			logAll.Print("Чёт пошло не так")
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		bodyReader, err := charset.NewReader(res.Body, res.Header.Get("Content-Type"))
-		if err != nil {
-			logAll.Print("Ошибка чтения страницы:", err)
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		textEsc, err := ioutil.ReadAll(bodyReader)
-		if err != nil {
-			logAll.Print("Ошибка чтения страницы:", err)
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		res.Body.Close()
-
-		text := html.UnescapeString(string(textEsc))
-
-		data, err := regexp.Compile("[0-9a-zA-Z-]+ [0-9:]{5}")
-		if err != nil {
-			logAll.Print("Не удалось создать правило для regexp")
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		date := data.FindString(text)
-
-		if (date != "") && (gkDate != date) {
-			err = scheduleNSU("GK")
-			if err == nil {
-				gkDate = date
-			}
-		}
-
-		res, err = http.Get("http://www.nsu.ru/education/schedule/Html_LK/Groups/")
-		if err != nil {
-			logAll.Print("Не удалось загрузить страницу:", err)
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		if res.Status != "200 OK" {
-			logAll.Print("Чёт пошло не так")
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		bodyReader, err = charset.NewReader(res.Body, res.Header.Get("Content-Type"))
-		if err != nil {
-			logAll.Print("Ошибка чтения страницы:", err)
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		textEsc, err = ioutil.ReadAll(bodyReader)
-		if err != nil {
-			logAll.Print("Ошибка чтения страницы:", err)
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		res.Body.Close()
-
-		text = html.UnescapeString(string(textEsc))
-
-		data, err = regexp.Compile("[0-9a-zA-Z-]+ [0-9:]{5}")
-		if err != nil {
-			logAll.Print("Не удалось создать правило для regexp")
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-
-		date = data.FindString(text)
-
-		if (date != "") && (lkDate != date) {
-			err = scheduleNSU("LK")
-			if err == nil {
-				lkDate = date
-			}
-		}
-
-		time.Sleep(time.Minute * 5)
-	}
 }
 
 func printSchedule(name string, offset int, id int) string {
@@ -171,10 +66,10 @@ func printSchedule(name string, offset int, id int) string {
 		}
 	}
 
-	v, ok := schedule[name]
+	v, ok := scheduleMap[name]
 	if !ok {
 		name += ".1"
-		v, ok = schedule[name]
+		v, ok = scheduleMap[name]
 		if !ok {
 			return "Неверно задан номер группы. Воспользуйтесь /help или /faq для помощи."
 		}
@@ -210,287 +105,6 @@ func printSchedule(name string, offset int, id int) string {
 	}
 
 	return textDay[(number+offset)%7] + "\n" + v[(number+offset)%7]
-}
-
-func scheduleNSU(group string) error {
-	res, err := http.Get("http://www.nsu.ru/education/schedule/Html_" + group + "/Groups/")
-	if err != nil {
-		logAll.Print("Не удалось загрузить страницу:", err)
-		return errors.New("Расписание временно недоступно.")
-	}
-
-	if res.Status != "200 OK" {
-		logAll.Print("Чёт пошло не так")
-		return errors.New("Не удалось найти такую группу.")
-	}
-
-	bodyReader, err := charset.NewReader(res.Body, res.Header.Get("Content-Type"))
-	if err != nil {
-		logAll.Print("Ошибка чтения страницы:", err)
-		return errors.New("Упс, что-то пошло не так.")
-	}
-
-	textEsc, err := ioutil.ReadAll(bodyReader)
-	if err != nil {
-		logAll.Print("Ошибка чтения страницы:", err)
-		return errors.New("Упс, что-то пошло не так.")
-	}
-
-	res.Body.Close()
-
-	text := html.UnescapeString(string(textEsc))
-
-	data, err := regexp.Compile("[0-9a-zA-Z-]+ [0-9:]{5}")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp")
-		return errors.New("Не удалось создать regexp data")
-	}
-
-	date := data.FindString(text)
-
-	hrefRegexp, err := regexp.Compile(">[0-9a-z]*_[0-9]*[.]htm")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp")
-		return errors.New("Не удалось создать regexp data")
-	}
-
-	hrefK := hrefRegexp.FindAllString(text, -1)
-
-	var mess [7]string
-	for i := 0; i < 7; i++ {
-		mess[i] = "Не удалось загрузить расписание, сообщите об этом."
-	}
-
-	for _, v := range hrefK {
-		err = parseTable(v[1:], group)
-		if err != nil {
-			logAll.Print("Не удалось выставить расписание: " + group)
-			schedule[v[1:]] = mess
-		}
-	}
-
-	if group == "GK" {
-		logAll.Print(date)
-		gkDate = date
-	} else {
-		logAll.Print(date)
-		lkDate = date
-	}
-
-	return nil
-}
-
-// parseTable
-func parseTable(name string, group string) error {
-	res, err := http.Get("http://old.nsu.ru/education/schedule/Html_" + group + "/Groups/" + name)
-	if err != nil {
-		logAll.Print("Не удалось загрузить страницу:", err)
-		return err
-	}
-
-	if res.Status != "200 OK" {
-		logAll.Println("Статус страницы не верен")
-		return errors.New("Статус страницы не верен.")
-	}
-
-	bodyReader, err := charset.NewReader(res.Body, res.Header.Get("Content-Type"))
-	if err != nil {
-		logAll.Print("Ошибка чтения страницы:", err)
-		return err
-	}
-
-	textEsc, err := ioutil.ReadAll(bodyReader)
-	if err != nil {
-		logAll.Print("Ошибка чтения страницы:", err)
-		return err
-	}
-
-	textIn := html.UnescapeString(string(textEsc))
-
-	title := parseTitle(textIn)
-
-	nameRegexp, err := regexp.Compile("[0-9]+[a-zA-Z0-9а-яА-Я][.][0-9]*")
-	if err != nil {
-		logAll.Print("Не смог сделать regexp:", err)
-		return err
-	}
-
-	groupTitle := nameRegexp.FindString(title)
-	if groupTitle == "" {
-		logAll.Print("Ошибка титула")
-		return errors.New("Ошибка титула")
-	}
-
-	text := []byte(textIn)
-
-	blocksRegexp, err := regexp.Compile("</TR>[^><]")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp")
-		return err
-	}
-
-	beginRegexp, err := regexp.Compile("<TD>")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp")
-		return err
-	}
-
-	endRegexp, err := regexp.Compile("</TD>")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp")
-		return err
-	}
-
-	n := blocksRegexp.FindAllIndex(text, -1)
-	if len(n) < 8 {
-		logAll.Print("Неверное количество блоков")
-		return errors.New("Неверное количество блоков")
-	}
-
-	var table [7][]byte
-	for i := 0; i < 7; i++ {
-		table[i] = text[n[i][1]:n[i+1][1]]
-	}
-
-	var tableDay [7][7][]byte
-	for k := 0; k < 7; k++ {
-
-		begin := beginRegexp.FindAllIndex(table[k], -1)
-		end := endRegexp.FindAllIndex(table[k], -1)
-		end = end[:]
-
-		var count, index int
-		for i := 1; i < len(begin); i++ {
-			if begin[i][1] > end[i][1] {
-				tableDay[k][count] = []byte(">" + string(table[k][begin[index][1]:end[i][0]]))
-				if end[i][0]-begin[index][1] == 2 {
-					tableDay[k][count] = []byte("-")
-				}
-				index = i
-				count++
-			}
-		}
-
-		if count != 7 {
-			tableDay[k][count] = []byte("-")
-		}
-	}
-
-	words, err := regexp.Compile(">[а-яА-Я][^a-zA-Z]+?<")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp")
-		return err
-	}
-
-	doubleDay, err := regexp.Compile("<HR")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp")
-		return err
-	}
-
-	for i := 0; i < 7; i++ {
-		for j := 0; j < 7; j++ {
-			result := words.FindAll(tableDay[i][j], -1)
-			resultIndex := words.FindAllIndex(tableDay[i][j], -1)
-
-			doubleDayIndex := doubleDay.FindIndex(tableDay[i][j])
-			doubleDayFlag := false
-
-			if len(doubleDayIndex) > 0 {
-				doubleDayFlag = true
-			}
-
-			var text string
-			var symbol string
-
-			for i, v := range result {
-				if doubleDayFlag && (resultIndex[i][0] > doubleDayIndex[0]) {
-					if i == 0 {
-						text += "-"
-					}
-
-					text += " <|> "
-					text += string(v[1:len(v)-1]) + ", "
-					doubleDayFlag = false
-				} else {
-					text += symbol + string(v[1:len(v)-1])
-					symbol = ", "
-				}
-			}
-
-			if len(doubleDayIndex) > 0 {
-				if doubleDayFlag {
-					text += " <|> -"
-				}
-			} else {
-				if text == "" {
-					text = "-"
-				}
-			}
-
-			tableDay[i][j] = []byte(text)
-		}
-	}
-
-	var message [7]string
-	for number := 0; number < 7; number++ {
-		message[number] = title + "\n" +
-			"1 П.  9:00: " + string(tableDay[0][number]) + "\n" +
-			"2 П. 10:50: " + string(tableDay[1][number]) + "\n" +
-			"3 П. 12:40: " + string(tableDay[2][number]) + "\n" +
-			"4 П. 14:30: " + string(tableDay[3][number]) + "\n" +
-			"5 П. 16:20: " + string(tableDay[4][number]) + "\n" +
-			"6 П. 18:10: " + string(tableDay[5][number]) + "\n" +
-			"7 П. 20:00: " + string(tableDay[6][number]) + "\n"
-	}
-
-	schedule[groupTitle] = message
-
-	return nil
-}
-
-// parseTitle
-func parseTitle(text string) string {
-	titleRegexp, err := regexp.Compile("<H1>.*</H1>")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp", err)
-		return ""
-	}
-
-	facRegexp, err := regexp.Compile(".*>.*</A>")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp", err)
-		return ""
-	}
-
-	facNameRegexp, err := regexp.Compile(">.*<")
-	if err != nil {
-		logAll.Print("Не удалось создать правило для regexp", err)
-		return ""
-	}
-
-	titleText := titleRegexp.FindString(text)
-
-	if len(titleText) > 5 {
-		titleText = titleText[4 : len(titleText)-5]
-	} else {
-		titleText = ""
-	}
-
-	facText := facRegexp.FindAllString(text, 2)
-
-	var facName string
-
-	if len(facText) > 1 {
-		facName = facNameRegexp.FindString(facText[1])
-		if len(facName) > 3 {
-			facName = facName[1 : len(facName)-1]
-		} else {
-			facName = ""
-		}
-	}
-
-	return facName + "\n" + titleText
 }
 
 // newChat Возвращает строку с новым каналом
@@ -628,16 +242,20 @@ func main() {
 
 	bot.Debug = false
 
-	err = scheduleNSU("GK")
+	info, err := schedule.GetAllSchedule(scheduleMap, "GK", &gkDate, &lkDate)
 	if err != nil {
 		bot.Send(tgbotapi.NewMessage(myId, "Всё плохо с GK"))
 		logAll.Panic("GK")
+	} else {
+		logAll.Print(info)
 	}
 
-	err = scheduleNSU("LK")
+	info, err = schedule.GetAllSchedule(scheduleMap, "LK", &gkDate, &lkDate)
 	if err != nil {
 		bot.Send(tgbotapi.NewMessage(myId, "Всё плохо с LK"))
 		logAll.Panic("LK")
+	} else {
+		logAll.Print(info)
 	}
 
 	go func() {
@@ -653,7 +271,20 @@ func main() {
 		}
 	}()
 
-	go parseSchedule()
+	go func() {
+		for {
+			answer, err := schedule.ParseSchedule(scheduleMap, &gkDate, &lkDate)
+			if err != nil {
+				logAll.Print(err)
+			} else {
+				if answer != "" {
+					logAll.Print(answer)
+				}
+			}
+
+			time.Sleep(5 * time.Minute)
+		}
+	}()
 
 	logAll.Printf("Бот %s запущен.", bot.Self.UserName)
 
--- schedule/schedule.go
+++ schedule/schedule.go
@@ -1,1 +1,355 @@
 package schedule
+
+import (
+	"errors"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/charset"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// searchFacultyName Вытаскивает из текста название факультета.
+func searchFacultyName(text string) (string, error) {
+	titleRegexp, err := regexp.Compile("<H1>.*</H1>")
+	if err != nil {
+		return "", errors.New("Не удалось создать правило для regexp")
+	}
+
+	facRegexp, err := regexp.Compile(".*>.*</A>")
+	if err != nil {
+		return "", errors.New("Не удалось создать правило для regexp")
+	}
+
+	facNameRegexp, err := regexp.Compile(">.*<")
+	if err != nil {
+		return "", errors.New("Не удалось создать правило для regexp")
+	}
+
+	titleText := titleRegexp.FindString(text)
+
+	if len(titleText) > 5 {
+		titleText = titleText[4 : len(titleText)-5]
+	} else {
+		titleText = ""
+	}
+
+	facText := facRegexp.FindAllString(text, 2)
+
+	var facName string
+
+	if len(facText) > 1 {
+		facName = facNameRegexp.FindString(facText[1])
+		if len(facName) > 3 {
+			facName = facName[1 : len(facName)-1]
+		} else {
+			facName = ""
+		}
+	}
+
+	return facName + "\n" + titleText, nil
+}
+
+func getGroupSchedule(scheduleMap map[string][7]string, name string, group string) error {
+	res, err := http.Get("http://old.nsu.ru/education/scheduleMap/Html_" + group + "/Groups/" + name)
+	if err != nil {
+		return err
+	}
+
+	if res.Status != "200 OK" {
+		return errors.New("Статус страницы не верен: " + res.Status)
+	}
+
+	bodyReader, err := charset.NewReader(res.Body, res.Header.Get("Content-Type"))
+	if err != nil {
+		return err
+	}
+
+	textEsc, err := ioutil.ReadAll(bodyReader)
+	if err != nil {
+		return err
+	}
+
+	textIn := html.UnescapeString(string(textEsc))
+
+	title, err := searchFacultyName(textIn)
+	if err != nil {
+		return err
+	}
+
+	nameRegexp, err := regexp.Compile("[0-9]+[a-zA-Z0-9а-яА-Я][.][0-9]*")
+	if err != nil {
+		return err
+	}
+
+	groupTitle := nameRegexp.FindString(title)
+	if groupTitle == "" {
+		return errors.New("Ошибка титула.")
+	}
+
+	text := []byte(textIn)
+
+	blocksRegexp, err := regexp.Compile("</TR>[^><]")
+	if err != nil {
+		return err
+	}
+
+	beginRegexp, err := regexp.Compile("<TD>")
+	if err != nil {
+		return err
+	}
+
+	endRegexp, err := regexp.Compile("</TD>")
+	if err != nil {
+		return err
+	}
+
+	n := blocksRegexp.FindAllIndex(text, -1)
+	if len(n) < 8 {
+		return errors.New("Неверное количество блоков")
+	}
+
+	var table [7][]byte
+	for i := 0; i < 7; i++ {
+		table[i] = text[n[i][1]:n[i+1][1]]
+	}
+
+	var tableDay [7][7][]byte
+	for k := 0; k < 7; k++ {
+
+		begin := beginRegexp.FindAllIndex(table[k], -1)
+		end := endRegexp.FindAllIndex(table[k], -1)
+		end = end[:]
+
+		var count, index int
+		for i := 1; i < len(begin); i++ {
+			if begin[i][1] > end[i][1] {
+				tableDay[k][count] = []byte(">" + string(table[k][begin[index][1]:end[i][0]]))
+				if end[i][0]-begin[index][1] == 2 {
+					tableDay[k][count] = []byte("-")
+				}
+				index = i
+				count++
+			}
+		}
+
+		if count != 7 {
+			tableDay[k][count] = []byte("-")
+		}
+	}
+
+	words, err := regexp.Compile(">[а-яА-Я][^a-zA-Z]+?<")
+	if err != nil {
+		return err
+	}
+
+	doubleDay, err := regexp.Compile("<HR")
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < 7; i++ {
+		for j := 0; j < 7; j++ {
+			result := words.FindAll(tableDay[i][j], -1)
+			resultIndex := words.FindAllIndex(tableDay[i][j], -1)
+
+			doubleDayIndex := doubleDay.FindIndex(tableDay[i][j])
+			doubleDayFlag := false
+
+			if len(doubleDayIndex) > 0 {
+				doubleDayFlag = true
+			}
+
+			var text string
+			var symbol string
+
+			for i, v := range result {
+				if doubleDayFlag && (resultIndex[i][0] > doubleDayIndex[0]) {
+					if i == 0 {
+						text += "-"
+					}
+
+					text += " <|> "
+					text += string(v[1:len(v)-1]) + ", "
+					doubleDayFlag = false
+				} else {
+					text += symbol + string(v[1:len(v)-1])
+					symbol = ", "
+				}
+			}
+
+			if len(doubleDayIndex) > 0 {
+				if doubleDayFlag {
+					text += " <|> -"
+				}
+			} else {
+				if text == "" {
+					text = "-"
+				}
+			}
+
+			tableDay[i][j] = []byte(text)
+		}
+	}
+
+	var message [7]string
+	for number := 0; number < 7; number++ {
+		message[number] = title + "\n" +
+			"1 П.  9:00: " + string(tableDay[0][number]) + "\n" +
+			"2 П. 10:50: " + string(tableDay[1][number]) + "\n" +
+			"3 П. 12:40: " + string(tableDay[2][number]) + "\n" +
+			"4 П. 14:30: " + string(tableDay[3][number]) + "\n" +
+			"5 П. 16:20: " + string(tableDay[4][number]) + "\n" +
+			"6 П. 18:10: " + string(tableDay[5][number]) + "\n" +
+			"7 П. 20:00: " + string(tableDay[6][number]) + "\n"
+	}
+
+	scheduleMap[groupTitle] = message
+
+	return nil
+}
+
+func GetAllSchedule(scheduleMap map[string][7]string, group string, gkDate *string, lkDate *string) (info string, err error) {
+	res, err := http.Get("http://www.nsu.ru/education/scheduleMap/Html_" + group + "/Groups/")
+	if err != nil {
+		return "", errors.New("Расписание временно недоступно.")
+	}
+
+	if res.Status != "200 OK" {
+		return "", errors.New("Страница работает некорректно: " + res.Status)
+	}
+
+	bodyReader, err := charset.NewReader(res.Body, res.Header.Get("Content-Type"))
+	if err != nil {
+		return "", errors.New("Не удалось отформатировать страницу.")
+	}
+
+	textEsc, err := ioutil.ReadAll(bodyReader)
+	if err != nil {
+		return "", errors.New("Не удалось считать содержаие body.")
+	}
+
+	res.Body.Close()
+
+	text := html.UnescapeString(string(textEsc))
+
+	data, err := regexp.Compile("[0-9a-zA-Z-]+ [0-9:]{5}")
+	if err != nil {
+		return "", err
+	}
+
+	date := data.FindString(text)
+
+	hrefRegexp, err := regexp.Compile(">[0-9a-z]*_[0-9]*[.]htm")
+	if err != nil {
+		return "", err
+	}
+
+	hrefK := hrefRegexp.FindAllString(text, -1)
+
+	var mess [7]string
+	for i := 0; i < 7; i++ {
+		mess[i] = "Не удалось загрузить расписание, сообщите об этом."
+	}
+
+	for _, v := range hrefK {
+		err = getGroupSchedule(scheduleMap, v[1:], group)
+		if err != nil {
+			info += group + " "
+			scheduleMap[v[1:]] = mess
+		}
+	}
+
+	if group == "GK" {
+		info = date + " " + info
+		*gkDate = date
+	} else {
+		info = date + " " + info
+		*lkDate = date
+	}
+
+	return info, nil
+}
+
+func ParseSchedule(scheduleMap map[string][7]string, gkDate *string, lkDate *string) (info string, err error) {
+	res, err := http.Get("http://www.nsu.ru/education/scheduleMap/Html_GK/Groups/")
+	if err != nil {
+		return "", err
+	}
+
+	if res.Status != "200 OK" {
+		return "", errors.New("Ошибка статуса страницы: " + res.Status)
+	}
+
+	bodyReader, err := charset.NewReader(res.Body, res.Header.Get("Content-Type"))
+	if err != nil {
+		return "", err
+	}
+
+	textEsc, err := ioutil.ReadAll(bodyReader)
+	if err != nil {
+		return "", err
+	}
+
+	err = res.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	text := html.UnescapeString(string(textEsc))
+
+	data, err := regexp.Compile("[0-9a-zA-Z-]+ [0-9:]{5}")
+	if err != nil {
+		return "", err
+	}
+
+	date := data.FindString(text)
+
+	if (date != "") && (*gkDate != date) {
+		mess, err := GetAllSchedule(scheduleMap, "GK", gkDate, lkDate)
+		if err == nil {
+			info = date + " " + mess
+			*gkDate = date
+		}
+	}
+
+	res, err = http.Get("http://www.nsu.ru/education/scheduleMap/Html_LK/Groups/")
+	if err != nil {
+		return "", err
+	}
+
+	if res.Status != "200 OK" {
+		return "", errors.New("Ошибка статуса страницы: " + res.Status)
+	}
+
+	bodyReader, err = charset.NewReader(res.Body, res.Header.Get("Content-Type"))
+	if err != nil {
+		return "", err
+	}
+
+	textEsc, err = ioutil.ReadAll(bodyReader)
+	if err != nil {
+		return "", err
+	}
+
+	res.Body.Close()
+
+	text = html.UnescapeString(string(textEsc))
+
+	data, err = regexp.Compile("[0-9a-zA-Z-]+ [0-9:]{5}")
+	if err != nil {
+		return "", err
+	}
+
+	date = data.FindString(text)
+
+	if (date != "") && (*lkDate != date) {
+		mess, err := GetAllSchedule(scheduleMap, "LK", gkDate, lkDate)
+		if err == nil {
+			info = date + " " + info + " " + mess
+			*lkDate = date
+		}
+	}
+
+	return info, nil
+}
