f05e78d7b749c8245947a53e53a58072fcac621c
Dimonchik0036 <dimonchik0036@gmail.com>
2017-03-20 02:49:07 +0700

    Начал делать парсер nsuhelp

---
+++ nsuhelp/nsuhelp.go
@@ -0,0 +1,89 @@
+package nsuhelp
+
+import (
+	"bufio"
+	"errors"
+	"golang.org/x/net/html"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+func GetLatestPosts() ([5]string, error) {
+	var er [5]string
+	res, err := http.Get("https://vk.com/nsuhelp")
+	if err != nil {
+		return er, err
+	}
+
+	if res.Status != "200 OK" {
+		return er, err
+	}
+
+	textBody, err := ioutil.ReadAll(bufio.NewReader(res.Body))
+	if err != nil {
+		return er, err
+	}
+
+	textReg, err := regexp.Compile("\"pi_text\">.+?</div>")
+	if err != nil {
+		return er, err
+	}
+
+	infoReg, err := regexp.Compile("\"wi_date\" href.+?>")
+	if err != nil {
+		return er, err
+	}
+
+	hrefReg, err := regexp.Compile("href=\".+?\"")
+	if err != nil {
+		return er, err
+	}
+
+	spaceReg, err := regexp.Compile("<.+?>")
+	if err != nil {
+		return er, err
+	}
+
+	text := html.UnescapeString(string(textBody))
+
+	postText := textReg.FindAllString(text, 5)
+	postHref := infoReg.FindAllString(text, 5)
+
+	brReg, err := regexp.Compile("<br/>")
+	if err != nil {
+		return er, err
+	}
+
+	
+	var result [5]string
+	for i, v := range postText {
+		if len(v) > 16 {
+			v = v[10 : len(v)-6]
+		} else {
+			return er, errors.New("Галюн сообщений")
+		}
+
+		if byte(v[0]) == ' ' {
+			v = v[1:]
+		}
+
+		for index := brReg.FindStringIndex(v); len(index) > 0; index = brReg.FindStringIndex(v) {
+			v = v[:index[0]] + "\n" + v[index[1]:]
+		}
+
+		for index := spaceReg.FindStringIndex(v); len(index) > 0; index = spaceReg.FindStringIndex(v) {
+			v = v[:index[0]] + v[index[1]:]
+		}
+
+		hrefText := hrefReg.FindString(postHref[i])
+
+		if len(hrefText) > 7 {
+			result[i] = v + "\n\n" + "vk.com" + hrefText[6:len(hrefText)-1] + "\n\n"
+		} else {
+			return er, errors.New("Галюн сообщений")
+		}
+	}
+
+	return result, nil
+}
--- test/test.go
+++ test/test.go
@@ -1,13 +1,102 @@
 package main
 
-import ()
+import (
+	"bufio"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"golang.org/x/net/html"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"TelegramBot/nsuhelp"
+)
 
-var users = make(map[int]string)
+const myId = 227605930
+const botToken = "325933326:AAFWjDWFPKFjAMg9MDr_Av-g643F_UhJmNY"
 
-func LoadUsers(users map[int]string) error {
+// SearchWeather Возвращает строку с температурой, в противном случае ошибку.
+func parseng() [5] string {
+	var er [5]string
+	res, err := http.Get("https://vk.com/nsuhelp?offset=10&own=1#posts")
+	if err != nil {
+		return er
+	}
 
-	return nil
+	if res.Status != "200 OK" {
+		return er
+	}
+
+	textBody, err := ioutil.ReadAll(bufio.NewReader(res.Body))
+	if err != nil {
+		return er
+	}
+
+	mess, err := regexp.Compile("\"pi_text\">.+?</div>")
+	wi_info, err := regexp.Compile("\"wi_date\" href.+?>")
+	hrefReg, err := regexp.Compile("href=\".+?\"")
+
+	text := html.UnescapeString(string(textBody))
+	print(text)
+
+	answer := mess.FindAllString(text, -1)
+	wi_answer := wi_info.FindAllString(text, -1)
+
+	brReg, err := regexp.Compile("<br/>")
+	if err != nil {
+		return er
+	}
+
+	trashReg, err := regexp.Compile("<.+?>")
+	if err != nil {
+		return er
+	}
+	var result [5]string
+	for i, v := range answer{
+		v = v[10 : len(v)-6]
+		if byte(v[0]) == ' ' {
+			v = v[1:]
+		}
+		for index := brReg.FindStringIndex(v); len(index) > 0; index = brReg.FindStringIndex(v) {
+			v = v[:index[0]] + "\n" + v[index[1]:]
+		}
+		for index := trashReg.FindStringIndex(v); len(index) > 0; index = trashReg.FindStringIndex(v) {
+			v = v[:index[0]] + v[index[1]:]
+		}
+		hrefText := hrefReg.FindString(wi_answer[i])
+
+		result[i] = v + "\n\n"+"vk.com"+hrefText[6:len(hrefText)-1]+"\n\n"
+	}
+
+
+
+	return result
 }
 
 func main() {
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		return
+	}
+
+	_, err = bot.Send(tgbotapi.NewMessage(myId, "Я перезагрузился."))
+	if err != nil {
+		return
+	}
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		return
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		a, _ := nsuhelp.GetLatestPosts()
+		for _, v := range a {
+			bot.Send(tgbotapi.NewMessage(myId, "Новый пост в \"Помогу в НГУ\":\n"+v))
+		}
+	}
 }
