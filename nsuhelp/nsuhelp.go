c7acaebf6cb2b5b57587c64f2c44a9f583d4f9b9
Dimonchik0036 <dimonchik0036@gmail.com>
2017-03-24 00:32:11 +0700

    Фикс рассылки новостей

--- nsuhelp/nsuhelp.go
+++ nsuhelp/nsuhelp.go
@@ -9,12 +9,18 @@ import (
 	"regexp"
 )
 
-var LatestPosts [5]string
+const CountPost = 5
+
+var LatestPosts [CountPost][2]string
 var UsersNsuHelp = make(map[int]bool)
 
-func GetLatestPosts() ([5]string, error) {
-	var er [5]string
-	res, err := http.Get("https://vk.com/nsuhelp")
+func GetLatestPosts(groupName string) ([CountPost][2]string, error) {
+	var er [CountPost][2]string
+	if groupName == "" {
+		return er, errors.New("Не задана группа.")
+	}
+
+	res, err := http.Get("https://vk.com/" + groupName)
 	if err != nil {
 		return er, err
 	}
@@ -38,6 +44,16 @@ func GetLatestPosts() ([5]string, error) {
 		return er, err
 	}
 
+	index1Reg, err := regexp.Compile("<div class=\"wall_item\"")
+	if err != nil {
+		return er, err
+	}
+
+	index2Reg, err := regexp.Compile("<div class=\"wi_buttons\"")
+	if err != nil {
+		return er, err
+	}
+
 	hrefReg, err := regexp.Compile("href=\".+?\"")
 	if err != nil {
 		return er, err
@@ -48,40 +64,67 @@ func GetLatestPosts() ([5]string, error) {
 		return er, err
 	}
 
-	text := html.UnescapeString(string(textBody))
-
-	postText := textReg.FindAllString(text, 5)
-	postHref := infoReg.FindAllString(text, 5)
-
 	brReg, err := regexp.Compile("<br/>")
 	if err != nil {
 		return er, err
 	}
 
-	var result [5]string
-	for i, v := range postText {
-		if len(v) > 16 {
-			v = v[10 : len(v)-6]
-		} else {
-			return er, errors.New("Галюн сообщений")
+	titleReg, err := regexp.Compile("<title>.*</title>")
+	if err != nil {
+		return er, err
+	}
+
+	text := html.UnescapeString(string(textBody))
+
+	titleText := titleReg.FindString(text)
+	if titleText == "" {
+		return er, errors.New("Отсутствует заголовок.")
+	} else {
+		titleText = titleText[7 : len(titleText)-8]
+	}
+
+	index1Text := index1Reg.FindAllStringIndex(text, CountPost)
+	index2Text := index2Reg.FindAllStringIndex(text, CountPost)
+
+	//log.Print(index1Text)
+	//log.Print(index2Text)
+
+	if len(index1Text) != CountPost || len(index2Text) != CountPost {
+		return er, errors.New("Мало постов.")
+	}
+
+	var result [CountPost][2]string
+
+	for i := 0; i < CountPost; i++ {
+		buffer := text[index1Text[CountPost-i-1][1]:index2Text[CountPost-i-1][0]]
+
+		result[i][0] = infoReg.FindString(buffer)
+		result[i][1] = textReg.FindString(buffer)
+
+		//log.Print(i, result[i][0]+"\n"+result[i][1])
+
+		if len(result[i][1]) > 16 {
+			result[i][1] = result[i][1][10 : len(result[i][1])-6]
 		}
 
-		if byte(v[0]) == ' ' {
-			v = v[1:]
+		if len(result[i][1]) > 0 {
+			if byte(result[i][1][0]) == ' ' {
+				result[i][1] = result[i][1][1:]
+			}
+
+			for index := brReg.FindStringIndex(result[i][1]); len(index) > 0; index = brReg.FindStringIndex(result[i][1]) {
+				result[i][1] = result[i][1][:index[0]] + "\n" + result[i][1][index[1]:]
+			}
+
+			for index := spaceReg.FindStringIndex(result[i][1]); len(index) > 0; index = spaceReg.FindStringIndex(result[i][1]) {
+				result[i][1] = result[i][1][:index[0]] + result[i][1][index[1]:]
+			}
 		}
 
-		for index := brReg.FindStringIndex(v); len(index) > 0; index = brReg.FindStringIndex(v) {
-			v = v[:index[0]] + "\n" + v[index[1]:]
-		}
-
-		for index := spaceReg.FindStringIndex(v); len(index) > 0; index = spaceReg.FindStringIndex(v) {
-			v = v[:index[0]] + v[index[1]:]
-		}
-
-		hrefText := hrefReg.FindString(postHref[i])
+		hrefText := hrefReg.FindString(result[i][0])
 
 		if len(hrefText) > 7 {
-			result[i] = v + "\n\n" + "vk.com" + hrefText[6:len(hrefText)-1] + "\n\n"
+			result[i][0] = titleText + "\nvk.com" + hrefText[6:len(hrefText)-1]
 		} else {
 			return er, errors.New("Галюн сообщений")
 		}
@@ -90,21 +133,38 @@ func GetLatestPosts() ([5]string, error) {
 	return result, nil
 }
 
-func GetNewPosts() []string {
-	p, err := GetLatestPosts()
+func GetNewPosts() (result [][2]string) {
+	p, err := GetLatestPosts("nsuhelp")
 	if err != nil {
 		return nil
 	}
 
-	if p[0] == LatestPosts[0] {
+	if (p[0][0] == LatestPosts[0][0]) && (p[CountPost-1][0] == LatestPosts[CountPost-1][0]) {
 		return nil
 	}
 
 	var index int
-	for ; (index < 5) && (p[0] != LatestPosts[index]); index++ {
+	for ; (index < CountPost) && (p[0][0] != LatestPosts[index][0]); index++ {
+	}
+	//log.Print("Индекс: ", index)
+
+	result = p[:index]
+
+	if index < CountPost && (p[CountPost-1][0] != LatestPosts[CountPost-1][0]) {
+		result = append(result, p[CountPost-1])
 	}
 
 	LatestPosts = p
 
-	return p[:index]
+	return
+}
+
+func GetGroupPost(groupName string) [CountPost][2]string {
+	p, err := GetLatestPosts(groupName)
+	if err != nil || p[0][0] == "" {
+		p[0][0] = "Не удалось сделать запрос в эту группу."
+		return p
+	}
+
+	return p
 }
--- test/test.go
+++ test/test.go
@@ -2,73 +2,12 @@ package main
 
 import (
 	"TelegramBot/nsuhelp"
-	"bufio"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"golang.org/x/net/html"
-	"io/ioutil"
-	"net/http"
-	"regexp"
 	"time"
 )
 
 const myId = 227605930
 const botToken = "325933326:AAFWjDWFPKFjAMg9MDr_Av-g643F_UhJmNY"
-
-// SearchWeather Возвращает строку с температурой, в противном случае ошибку.
-func parseng() [5]string {
-	var er [5]string
-	res, err := http.Get("https://vk.com/nsuhelp?offset=10&own=1#posts")
-	if err != nil {
-		return er
-	}
-
-	if res.Status != "200 OK" {
-		return er
-	}
-
-	textBody, err := ioutil.ReadAll(bufio.NewReader(res.Body))
-	if err != nil {
-		return er
-	}
-
-	mess, err := regexp.Compile("\"pi_text\">.+?</div>")
-	wi_info, err := regexp.Compile("\"wi_date\" href.+?>")
-	hrefReg, err := regexp.Compile("href=\".+?\"")
-
-	text := html.UnescapeString(string(textBody))
-	print(text)
-
-	answer := mess.FindAllString(text, -1)
-	wi_answer := wi_info.FindAllString(text, -1)
-
-	brReg, err := regexp.Compile("<br/>")
-	if err != nil {
-		return er
-	}
-
-	trashReg, err := regexp.Compile("<.+?>")
-	if err != nil {
-		return er
-	}
-	var result [5]string
-	for i, v := range answer {
-		v = v[10 : len(v)-6]
-		if byte(v[0]) == ' ' {
-			v = v[1:]
-		}
-		for index := brReg.FindStringIndex(v); len(index) > 0; index = brReg.FindStringIndex(v) {
-			v = v[:index[0]] + "\n" + v[index[1]:]
-		}
-		for index := trashReg.FindStringIndex(v); len(index) > 0; index = trashReg.FindStringIndex(v) {
-			v = v[:index[0]] + v[index[1]:]
-		}
-		hrefText := hrefReg.FindString(wi_answer[i])
-
-		result[i] = v + "\n\n" + "vk.com" + hrefText[6:len(hrefText)-1] + "\n\n"
-	}
-
-	return result
-}
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(botToken)
@@ -91,18 +30,21 @@ func main() {
 
 	go func() {
 		for {
+			time.Sleep(59 * time.Second)
+
 			a := nsuhelp.GetNewPosts()
 			if len(a) != 0 {
-				for i, b := range nsuhelp.UsersNsuHelp {
-					if b {
-						for _, v := range a {
-							bot.Send(tgbotapi.NewMessage(int64(i), "Новый пост в \"Помогу в НГУ\":\n\n"+v))
+				if a[0][0] != "" {
+					for i, b := range nsuhelp.UsersNsuHelp {
+						if b {
+							for _, v := range a {
+								bot.Send(tgbotapi.NewMessage(int64(i), v[0]+"\n\n"+v[1]))
+							}
 						}
 					}
+
 				}
 			}
-
-			time.Sleep(30 * time.Second)
 		}
 	}()
 
@@ -113,21 +55,19 @@ func main() {
 
 		if update.Message.IsCommand() {
 			switch update.Message.Command() {
-			case "nsuhelpon":
-				_, ok := nsuhelp.UsersNsuHelp[update.Message.From.ID]
+			case "nsuhelp":
+				v, ok := nsuhelp.UsersNsuHelp[update.Message.From.ID]
 				if !ok {
 					nsuhelp.UsersNsuHelp[update.Message.From.ID] = true
 					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Вы были подписаны на рассылку."))
 				} else {
-					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Вы уже подписаны на рассылку."))
-				}
-			case "nsuhelpoff":
-				_, ok := nsuhelp.UsersNsuHelp[update.Message.From.ID]
-				if !ok {
-					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Вы не были подписаны на рассылку."))
-				} else {
-					delete(nsuhelp.UsersNsuHelp, update.Message.From.ID)
-					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Вы были отписаны от рассылки."))
+					if v {
+						delete(nsuhelp.UsersNsuHelp, update.Message.From.ID)
+						bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Вы были отписаны от рассылки."))
+					} else {
+						nsuhelp.UsersNsuHelp[update.Message.From.ID] = true
+						bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Вы были подписаны на рассылку."))
+					}
 				}
 			case "check":
 				switch update.Message.From.ID {
@@ -139,6 +79,15 @@ func main() {
 					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Жека, не дудось!\n\nP.S. Кирилл пидор."))
 				default:
 					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Дай копейку на дошик одмину."))
+				}
+			case "post":
+				a := nsuhelp.GetGroupPost(update.Message.CommandArguments())
+				if len(a) != 0 {
+					if a[0][0] != "" {
+						for _, v := range a {
+							bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, v[0]+"\n\n"+v[1]))
+						}
+					}
 				}
 			}
 		}
